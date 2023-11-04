2092bc518a3ef1914aae26d4915b7cc42f32af75
gsistelos <gabrielsistelos@gmail.com>
2023-11-04 00:04:34 -0300

    fix: conflicting server name warn message

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -53,7 +53,7 @@ void Server::configToServerName(ConfigBlock& configBlock) {
         std::map<std::string, ConfigBlock*>::iterator found = this->_configs.find(*it);
 
         if (found != this->_configs.end()) {
-            std::cerr << "nginx: [warn] conflicting server name \"" << *it << "\" on localhost:" << this->_hostPort.port << ", ignored" << std::endl;
+            std::cerr << "webserv: [warn] conflicting server name \"" << *it << "\" on localhost:" << this->_hostPort.port << ", ignored" << std::endl;
             continue;
         }
 
