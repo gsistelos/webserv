e180b9d776e0acc9189c2f2a7adf619afa613aa5
Vitor Sergio <vitorsergio111@gmail.com>
2023-11-06 23:07:52 -0300

    feat: improve search script

--- cgi-bin/search.py
+++ cgi-bin/search.py
@@ -7,6 +7,17 @@ form = cgi.FieldStorage()
 
 filename = form.getvalue('filename')
 
+fail_message = b"<html>"
+fail_message += b"<p>No file was uploaded</p>"
+fail_message += b"</html>"
+
+if (filename == None):
+	sys.stdout.buffer.write(b"HTTP/1.1 400 Bad Request")
+	sys.stdout.buffer.write(b"Content-Length: " + str(len(fail_message)).encode("utf-8") + b"\r\n")
+	sys.stdout.buffer.write(b"\r\n")
+	sys.stdout.buffer.write(fail_message)
+	sys.exit()
+
 if os.path.exists("cgi-bin/uploads/" + filename):
 	file_content = open("cgi-bin/uploads/" + filename, "rb").read()
 
@@ -15,11 +26,7 @@ if os.path.exists("cgi-bin/uploads/" + filename):
 	sys.stdout.buffer.write(b"\r\n")
 	sys.stdout.buffer.write(file_content)
 else:
-	response = b"<html>"
-	response += b"<p>No file was uploaded</p>"
-	response += b"</html>"
-
 	sys.stdout.buffer.write(b"HTTP/1.1 400 Bad Request")
-	sys.stdout.buffer.write(b"Content-Length: " + str(len(response)).encode("utf-8") + b"\r\n")
+	sys.stdout.buffer.write(b"Content-Length: " + str(len(fail_message)).encode("utf-8") + b"\r\n")
 	sys.stdout.buffer.write(b"\r\n")
-	sys.stdout.buffer.write(response)
+	sys.stdout.buffer.write(fail_message)
