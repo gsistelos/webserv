4babac5b438d1072d868d7d6f57a40cefb456872
Marlon Reis <72058662+expoilderk@users.noreply.github.com>
2023-11-10 20:26:50 -0300

    fix: fix search script syntax

--- cgi-bin/search.py
+++ cgi-bin/search.py
@@ -7,6 +7,17 @@ form = cgi.FieldStorage()
 
 filename = form.getvalue('filename')
 
+fail_message = b"<html>"
+fail_message += b"<p>No file was uploaded</p>"
+fail_message += b"</html>"
+
+if (filename == None):
+	sys.stdout.buffer.write(b"HTTP/1.1 400 Bad Request\r\n")
+	sys.stdout.buffer.write(b"Content-Length: " + str(len(fail_message)).encode("utf-8") + b"\r\n")
+	sys.stdout.buffer.write(b"\r\n")
+	sys.stdout.buffer.write(fail_message)
+	sys.exit()
+
 if os.path.exists("cgi-bin/uploads/" + filename):
 	file_content = open("cgi-bin/uploads/" + filename, "rb").read()
 
@@ -14,3 +25,8 @@ if os.path.exists("cgi-bin/uploads/" + filename):
 	sys.stdout.buffer.write(b"Content-Length: " + str(len(file_content)).encode("utf-8") + b"\r\n")
 	sys.stdout.buffer.write(b"\r\n")
 	sys.stdout.buffer.write(file_content)
+else:
+	sys.stdout.buffer.write(b"HTTP/1.1 400 Bad Request\r\n")
+	sys.stdout.buffer.write(b"Content-Length: " + str(len(fail_message)).encode("utf-8") + b"\r\n")
+	sys.stdout.buffer.write(b"\r\n")
+	sys.stdout.buffer.write(fail_message)
